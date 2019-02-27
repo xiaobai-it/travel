4c5017b5b3340415ee178c6234e051e9347c8692
qwd_global <qwd_global@qq.com>
2019-02-27 15:07:34 +0800

    项目初始化2

---
+++ src/pages/home/home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      home页面
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: ''
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
