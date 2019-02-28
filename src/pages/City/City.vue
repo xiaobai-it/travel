4090291eeee15207bc8a156688968a84436b85f2
qwd_global <qwd_global@qq.com>
2019-02-28 18:32:12 +0800

    city列表布局静态搭建完成

--- src/pages/City/City.vue
+++ src/pages/City/City.vue
@@ -2,12 +2,16 @@
     <div>
       <cityHeader/>
       <citySearch/>
+      <cityList/>
+      <cityName/>
     </div>
 </template>
 
 <script>
   import cityHeader from '../../components/City/Header/Header'
   import citySearch from '../../components/City/Search/Search'
+  import cityList from '../../components/City/List/List'
+  import cityName from '../../components/City/CityName/CityName'
 
   export default {
     data() {
@@ -17,7 +21,9 @@
     },
     components:{
       cityHeader,
-      citySearch
+      citySearch,
+      cityList,
+      cityName
     }
   }
 </script>
